f26342e1d3693a0feb5b3dbe1779b709e532e6a8
davidnene <davienesh4@gmail.com>
2022-06-05 21:57:40 +0300

    commit tree 4728ddf927e397e0e010d0d8f98b3efb626c81b5

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+const input = document.getElementById("input");
+
+function clickAlert() {
+    alert('I was clicked!');
 }
+function addingEventListener() {
+    input.addEventListener('click', clickAlert);
+}
+
+console.log(addingEventListener())
+/*
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+  */
